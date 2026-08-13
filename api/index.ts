81698445cfbf526d6a27ab21f3c23f660eaa5710
Saman <samansajid0158@gmail.com>
2026-08-14 03:17:26 +0500

    Add Vercel serverless entry point

---
+++ api/index.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import serverlessExpress from '@vendia/serverless-express';
+import express from 'express';
+import { AppModule } from '../src/app.module';
+
+let cachedServer;
+
+async function bootstrapServer() {
+  if (!cachedServer) {
+    const expressApp = express();
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+    nestApp.enableCors();
+    await nestApp.init();
+    cachedServer = serverlessExpress({ app: expressApp });
+  }
+  return cachedServer;
+}
+
+export default async (req, res) => {
+  const server = await bootstrapServer();
+  return server(req, res);
+};
